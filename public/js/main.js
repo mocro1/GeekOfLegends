f49750faccaf1436a66b7ea44d278341e61cac70
mocro1 <ahmedadda2001@gmail.com>
2023-01-06 14:15:16 +0100

    debut projet

---
+++ public/js/main.js
@@ -0,0 +1,4 @@
+import { boss1 } from "./module/class.js";
+import * as CLASS from "./module/class.js";
+import {Boss} from "./module/class.js"
+console.log(boss1);
---
+++ public/js/module/class.js
@@ -0,0 +1,9 @@
+export class Boss{
+    constructor(nom,nbrPntVie,nbrPntAttaque){
+        this.nom = nom;
+        this.nbrPntVie = nbrPntVie;
+        this.nbrPntAttaque = nbrPntAttaque
+    }
+}
+
+export let boss1 = new Boss("Cactus","Kbib");
